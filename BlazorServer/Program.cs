50ee5c3b6466a4554a714b02f974902b6cad7727
Dan <danielfr500@gmail.com>
2022-10-03 00:24:43 -0400

    Add project files.

---
+++ BlazorServer/Program.cs
@@ -0,0 +1,47 @@
+using BlazorServer.Data;
+using Microsoft.AspNetCore.ResponseCompression;
+using BlazorServer.Hubs;
+
+namespace BlazorServer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddSingleton<WeatherForecastService>();
+            builder.Services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    new[] { "application/octet-stream" }); //Gives ability to process octetstream with compression 
+            });
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.MapBlazorHub();
+            app.MapHub<ChatHub>("/ChatHub"); //creates signalR /chathub url path to connect
+            app.MapFallbackToPage("/_Host");
+
+            app.Run();
+        }
+    }
+}
---
+++ WPFClient/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿
+using System.Windows;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace WPFClient;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+
+    HubConnection connection;
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7216/chathub")
+            .WithAutomaticReconnect()
+            .Build();
+
+
+        connection.Reconnecting += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Attempting to reconnect...";
+                messages.Items.Add(newMessage); //adding to listbox in Mainwindow
+            });
+            return Task.CompletedTask;
+        };
+
+        connection.Reconnected += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Reconnected to the Server...";
+                messages.Items.Clear();
+                messages.Items.Add(newMessage); //adding to listbox in Mainwindow
+            });
+            return Task.CompletedTask;
+        };
+
+        connection.Closed += (sender) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = "Connection Closed";
+                messages.Items.Add(newMessage); //adding to listbox in Mainwindow
+                openConnection.IsEnabled = true;
+                sendMessage.IsEnabled = false; //button is disabled can't send a message if connectio nis closed
+            
+            
+            });
+            return Task.CompletedTask;
+        };
+
+
+
+    }
+
+    private async void openConnection_Click(object sender, RoutedEventArgs e)
+    {
+        connection.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                var newMessage = $"{user}: {message}";
+                messages.Items.Add(newMessage); //adding to listbox in Mainwindow
+            });
+        });
+
+
+        //protects against trying a connection that doesn't work
+        try 
+        {
+            await connection.StartAsync();
+            messages.Items.Add("Connection Started");
+            openConnection.IsEnabled = false; //disables button
+            sendMessage.IsEnabled = true;
+        }
+        catch(Exception ex)
+        {
+            messages.Items.Add(ex.Message); //adds exception into list of messages
+        }
+    }
+
+    private async void sendMessage_Click(object sender, RoutedEventArgs e)
+    {
+        try
+        {
+            //WPF client is hard coded
+            await connection.InvokeAsync("SendMessage", 
+                "WPF Client", messageInput.Text);
+        }
+        catch(Exception ex)
+        {
+            messages.Items.Add(ex.Message); //adds exception into list of messages
+
+        }
+
+
+
+    }
+}
